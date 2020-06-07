ff6290dc1c94008fcd8c54d20e76ba48c4db0fe7
Yuri Duarte <yuriduartedev@gmail.com>
2020-06-07 18:03:17 -0300

    Criando validação de dados no back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import routes from './routes';
+import { errors } from 'celebrate';
 
 const app = express();
 
@@ -9,14 +10,5 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
-
-const users = [
-  'Yuri',
-  'Cleiton',
-  'Robson',
-  'Daniel'
-]
-
-
-
+app.use(errors());
 app.listen(3333);
