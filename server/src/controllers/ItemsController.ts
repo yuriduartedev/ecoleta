a24cc6bf83c985eeba7723589d3f5207c7a8a8e2
Yuri Duarte <yuriduartedev@gmail.com>
2020-06-07 11:09:41 -0300

    Adiciona projeto back-end e mobile no controle de versão

---
+++ server/src/controllers/ItemsController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection';
+
+// PADRÃO DE CRIAÇÂO de MÉTODOS DENTRO DO CONTROLLER
+// index, show, create, update, delete
+
+class ItemsController {
+  async index(request: Request, response: Response) {
+    const items = await knex('items').select('*');
+
+    const serializedItems = items.map(item => {
+      return {
+        id: item.id,
+        title: item.title,
+        image_url: `http://localhost:3333/uploads/${item.image}`
+      }
+    })
+    return response.json(serializedItems);
+  }
+}
+
+export default ItemsController;
